b08a9ba5284dd9387990ab75707e93cfc1669aea
ChibuikemLucas <chibuikemlucas@gmail.com>
2024-11-19 21:32:33 +0100

    Creating homepage and custom button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,8 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
 import state from '../store';
+import { CustomButton } from '../components';
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -10,8 +11,44 @@ import {
 } from '../config/motion';
 
 const Home = () => {
+  const snap =useSnapshot(state);
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {snap.intro &&(
+        <motion.section className="home" {...slideAnimation('left')}>
+           <motion.header {...slideAnimation('down')}>
+            <img 
+              src = './threejs.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+           </motion.header>
+
+           <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+               <h1 className="head-text">
+                 LET'S <br className="xl:block hidden" /> DO IT.
+               </h1>
+            </motion.div>
+            <motion.div 
+              {...headContainerAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base"> 
+                Create your unique and exclusive shirt with our
+                 brand-new 3D customization tool. 
+                <strong>Unleash your imagination</strong>{" "} 
+              and define your own style.</p>
+
+              <CustomButton 
+              
+              />
+            </motion.div>
+           </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
