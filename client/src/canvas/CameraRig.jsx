6751ff5c11ca7a2434b5ab88b293b9c92305164c
ChibuikemLucas <chibuikemlucas@gmail.com>
2024-11-24 18:13:26 +0100

    Set model camera position

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -9,31 +9,33 @@ const CameraRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
 
-  useFrame((state, delta)=>{
+  useFrame((state, delta) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
 
-    // set the initial position of the model
-    let largePosition =[-0.4, 0, 2];
-    if(snap.intro) {
-      if(isBreakpoint) targetPosition = [0, 0, 2];
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+    // Set the initial position of the model
+    let targetPosition = [-0.4, 0, 2];
+    if (snap.intro) {
+      if (isBreakpoint) targetPosition = [0, 0, 2];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
     } else {
-      if(isMobile) targetPosition = [0, 0.2, 2.5];
+      if (isMobile) targetPosition = [0, 0.2, 2.5];
       else targetPosition = [0, 0, 2];
     }
 
-    //set camer
+    // Set model camera position
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
 
-  //Set model rotation smoothly
-easing.dampE(
-  group.current.rotation,
-  [state.pointer.y / 10, -state.pointer.x / 5, 0],
-  0.25,
-  delta
-)  
-  })
-  return <group>{children}</group>
-}
+    // Set model rotation smoothly
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
 
-export default CameraRig
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
