c0602c311b4a8a008b7153a5f78f2aa979f74538
ChibuikemLucas <chibuikemlucas@gmail.com>
2025-01-10 17:39:30 +0100

    Complete server setup

--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limig: "50mb" }))
+app.use(express.json({ limit: "50mb" })); // Corrected typo here
 
 app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', (req, res) => {
-    res.status(200).json({ message: "Hello World" })
+    res.status(200).json({ message: "Hello World" });
 });
 
-app.listen(5000, () => console.log('Server has started on port 5000'))
+app.listen(5000, () => console.log('Server has started on port 5000'));
